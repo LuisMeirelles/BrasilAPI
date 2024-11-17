bff2804bf654d3541bb763243bac0fc6b812e599
Luis Meirelles <luis.gustavo@essentia.com.br>
2024-11-17 01:19:41 -0300

    refactor: guarantee the `errors` array is always present, even with a single error

--- services/pix/brcode/checkAndReturnField.js
+++ services/pix/brcode/checkAndReturnField.js
@@ -22,7 +22,10 @@ import BadRequestError from '@/errors/BadRequestError';
  * @returns {string}
  */
 export const checkAndReturnField = (field, value) => {
-  const errors = [];
+  const error = new BadRequestError({
+    message: `O campo '${field.description}' não é válido.`,
+    errors: [],
+  });
 
   if (field.value) {
     return field.value;
@@ -33,48 +36,32 @@ export const checkAndReturnField = (field, value) => {
       return '';
     }
 
-    throw new BadRequestError({
-      message: `O campo '${field.description}' é obrigatório.`,
-    });
+    error.errors.push('É obrigatório.');
+    throw error; // se é obrigatório e não foi informado não faz sentido continuar
   }
 
   if (field.length && value.length !== field.length) {
-    errors.push(
-      `O campo '${field.description}' deve ter exatamente ${field.length} caracteres.`
-    );
+    error.errors.push(`Deve ter exatamente ${field.length} caracteres.`);
   }
 
   if (field.minLength && value.length < field.minLength) {
-    errors.push(
-      `O campo '${field.description}' deve ter no mínimo ${field.minLength} caracteres.`
-    );
+    error.errors.push(`Deve ter no mínimo ${field.minLength} caracteres.`);
   }
 
   if (field.maxLength && value.length > field.maxLength) {
-    errors.push(
-      `O campo '${field.description}' deve ter no máximo ${field.maxLength} caracteres.`
-    );
+    error.errors.push(`Deve ter no máximo ${field.maxLength} caracteres.`);
   }
 
   if (typeof field.validate === 'function') {
     const fieldValid = field.validate(value);
 
     if (!fieldValid) {
-      errors.push(
-        `O campo '${field.description}' é inválido (validação personalizada).`
-      );
+      error.errors.push(`Não passou na validação personalizada.`);
     }
   }
 
-  if (errors.length === 1) {
-    throw new BadRequestError({ message: errors[0] });
-  }
-
-  if (errors.length > 1) {
-    throw new BadRequestError({
-      message: `O campo '${field.description}' não é válido.`,
-      errors,
-    });
+  if (error.errors.length > 0) {
+    throw error;
   }
 
   return value;
