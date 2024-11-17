c47fe552bd7d0753bf49e7c1a1be97f7cb47d654
Luis Meirelles <luis.gustavo@essentia.com.br>
2024-11-17 20:58:50 -0300

    test: update `testCorsForRoute` helper to allow any method.
    the method and payload are optional, defaulting to GET and undefined, respectively

--- tests/helpers/cors.js
+++ tests/helpers/cors.js
@@ -1,21 +1,26 @@
 const axios = require('axios');
 
-const testCorsForRoute = (route) => {
+const testCorsForRoute = (route, method = 'GET', payload = undefined) => {
   describe(`CORS Middleware for ${route}`, () => {
     const url = `${global.SERVER_URL}${route}`;
 
-    it('deve permitir solicitações da origem permitida', async () => {
-      const response = await axios.get(url, {
+    it(`deve permitir solicitações da origem permitida usando ${method}`, async () => {
+      const response = await axios({
+        url,
+        method,
+        data: payload,
         headers: { Origin: 'http://exemplo.com' },
       });
+
       expect(response.headers['access-control-allow-origin']).toBe('*');
+      expect(response.status).toBe(method === 'OPTIONS' ? 204 : 200);
     });
 
     it('deve lidar corretamente com pre-flight CORS request', async () => {
       const response = await axios.options(url, {
         headers: {
           Origin: 'http://exemplo.com',
-          'Access-Control-Request-Method': 'GET',
+          'Access-Control-Request-Method': method,
           'Access-Control-Request-Headers': 'Content-Type, Authorization',
         },
       });
@@ -33,23 +38,27 @@ const testCorsForRoute = (route) => {
       expect(response.status).toBe(204);
     });
 
-    it('deve permitir métodos específicos', async () => {
+    it(`deve permitir o método ${method} no pre-flight`, async () => {
       const response = await axios.options(url, {
         headers: {
           Origin: 'http://exemplo.com',
-          'Access-Control-Request-Method': 'GET',
+          'Access-Control-Request-Method': method,
         },
       });
-      expect(response.headers['access-control-allow-methods']).toContain('GET');
+
+      expect(response.headers['access-control-allow-methods']).toContain(
+        method
+      );
     });
 
-    it('deve permitir cabeçalhos específicos', async () => {
+    it(`deve permitir cabeçalhos específicos para ${method}`, async () => {
       const response = await axios.options(url, {
         headers: {
           Origin: 'http://exemplo.com',
           'Access-Control-Request-Headers': 'Content-Type',
         },
       });
+
       expect(response.headers['access-control-allow-headers']).toContain(
         'Content-Type'
       );
